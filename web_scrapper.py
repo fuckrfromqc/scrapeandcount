c729d19d60fb8db53c6e233f66559d15c9c6c60a
fuckrfromqc <120679133+fuckrfromqc@users.noreply.github.com>
2023-04-01 18:39:05 -0400

    Update web_scrapper.py

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,26 +1,25 @@
 from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
-import re
 
 app = Flask(__name__)
 
-def count_occurrences(url, search_text):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.get_text()
-    pattern = re.compile(search_text, re.IGNORECASE)
-    matches = pattern.findall(text)
-    return len(matches)
+def count_occurrences(word, soup):
+    text = soup.get_text().lower()
+    words = text.split()
+    return words.count(word.lower())
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     count = None
-    if request.method == 'POST':
-        url = request.form['url']
-        search_text = request.form['search_text']
-        count = count_occurrences(url, search_text)
-    return render_template('index.html', count=count)
+    if request.method == "POST":
+        url = request.form["url"]
+        search_text = request.form["search_text"]
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        count = count_occurrences(search_text, soup)
+
+    return render_template("index.html", count=count)
 
 if __name__ == "__main__":
     app.run(debug=True)
